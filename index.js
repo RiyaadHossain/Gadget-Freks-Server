5e0209e64c00b9ea9f73375b33a213ce0cc647e1
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-29 11:04:30 +0600

    Product added

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const cors = require('cors');
+require('dotenv').config()
 const app = express()
 const port = 4000
 
 // Middlewares -
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@gadgetfreks.nqro1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    await client.connect()
+    const productCollection = client.db('AllProducts').collection('products')
+
+    app.post('/addproduct', async (req, res) => {
+      const product = req.body
+      const result = await productCollection.insertOne(product)
+      res.send(result)
+    })
+  }
+  finally {
+    
+  }
+}
+
+run().catch(console.dir)
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
